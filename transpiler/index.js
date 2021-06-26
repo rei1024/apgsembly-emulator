96354fd0b87e528c2711491b51a8683ba3ece450
rei1024 <satopen1729@gmail.com>
2021-06-26 22:34:18 +0900

    add transpiler

---
+++ transpiler/index.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+import { Program } from "../src/Program.js";
+
+// Transpiler
+
+const $input = document.querySelector('#input');
+if (!($input instanceof HTMLTextAreaElement)) {
+    throw Error('input is not a HTMLTextAreaElement');
+}
+
+const $output = document.querySelector('#output');
+if (!($output instanceof HTMLTextAreaElement)) {
+    throw Error('input is not a HTMLTextAreaElement');
+}
+
+const $transpile = document.querySelector('#transpile');
+if (!($transpile instanceof HTMLElement)) {
+    throw Error('input is not a HTMLTextAreaElement');
+}
+
+$transpile.addEventListener('click', () => {
+    const program = Program.parse($input.value);
+    if (program instanceof Program) {
+        $output.value = $output.value;
+    } else {
+        $output.value = program;
+    }
+});
