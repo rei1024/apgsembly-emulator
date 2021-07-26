02f233d7c10990af2eb5c255f6e9cf35559dc15a
rei1024 <satopen1729@gmail.com>
2021-07-26 11:50:51 +0900

    add e2e tests

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,30 +1,85 @@
-describe('Test', () => {
-    it('Pi calculator', () => {
-        cy.visit('http://localhost:1123/');
+const apgsemblyEmulatorPageURL = 'http://localhost:1123/';
+
+describe('Load', () => {
+    it('should load', () => {
+        cy.visit(apgsemblyEmulatorPageURL);
+        cy.get('#start').should('not.be.disabled');
+    });
+});
+
+describe('Error', () => {
+    it('Error', () => {
+        cy.visit(apgsemblyEmulatorPageURL);
         cy.contains('APGsembly');
-        cy.get('#samples').click();
-        cy.get('[data-src="pi_calc.apg"]').click();
+        cy.get('#start').click();
+        cy.contains('Program is empty');
+    });
+});
+
+/**
+ *
+ * @param {string} str 
+ */
+function loadProgram(str) {
+    cy.get('#samples').click();
+    cy.get(`[data-src="${str}"]`).click();
+}
+
+/**
+ *
+ * @param {number} n 
+ */
+function setStep(n) {
+    cy.contains('Config').click();
+    cy.wait(100);
+    cy.get('#step_input').type(`{selectall}{backspace}${n}`);
+    cy.wait(100);
+    cy.get('#config_modal .btn-close').click();
+    cy.wait(100);
+}
+
+describe('Integers', () => {
+    it('should print integers', () => {
+        cy.visit(apgsemblyEmulatorPageURL);
+        cy.contains('APGsembly');
+        loadProgram('integers.apg');
+
+        setStep(1050);
+        cy.get('#step').click();
+        cy.get('#output').should('have.value', '1.2.3.4.5.6.7.8.9.10');
+
+        cy.get('#steps').should('have.text', '1050');
+    });
+});
+
+describe('Pi calculator', () => {
+    it('should print pi', () => {
+        cy.visit(apgsemblyEmulatorPageURL);
+        cy.contains('APGsembly');
+        loadProgram('pi_calc.apg');
 
         cy.contains('U0');
         cy.contains('U9');
         cy.contains('B0');
         cy.contains('B3');
 
-        cy.contains('Config').click();
-        cy.wait(100);
-        cy.get('#step_input').type('{backspace}1000000');
-        cy.get('#config_modal .btn-close').click();
-        cy.wait(100);
+        setStep(1000000);
         cy.get('#step').click();
         cy.get('#output').should('have.value', '3.141');
 
         cy.get('#steps').should('have.text', '1000000');
     });
+});
 
-    it('Error', () => {
-        cy.visit('http://localhost:1123/');
+describe('Rule 110', () => {
+    it('Rule 110 should work', () => {
+        cy.visit(apgsemblyEmulatorPageURL);
         cy.contains('APGsembly');
-        cy.get('#start').click();
-        cy.contains('Program is empty');
+        loadProgram('rule110.apg');
+
+        setStep(1000);
+        cy.get('#step').click();
+
+        cy.get('#steps').should('have.text', '1000');
     });
 });
