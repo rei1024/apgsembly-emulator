071b3de234a120282358eb1cb712bef5e13bf2b9
rei1024 <satopen1729@gmail.com>
2022-09-23 01:45:50 +0900

    refactor parsing

--- src/ProgramLines.js
+++ src/ProgramLines.js
@@ -3,6 +3,7 @@
 import {
     Command,
     ProgramLine,
+    parseProgramLine,
 } from "./Command.js";
 
 /**
@@ -46,7 +47,7 @@ export class ProgramLines {
     static parse(str) {
         const lines = str.split(/\r\n|\n|\r/u);
 
-        const programLineWithErrorArray = lines.map((line, index) => Command.parse(line, index + 1));
+        const programLineWithErrorArray = lines.map((line, index) => parseProgramLine(line, index + 1));
 
         const errors = programLineWithErrorArray
             .flatMap(x => typeof x === 'string' ? [x] : []);
