f5b9ee719ec66f65f67401b0bad72736975fcdf9
rei1024 <satopen1729@gmail.com>
2022-10-15 16:44:40 +0900

    add link to mermaid

--- labs/diagram/index.js
+++ labs/diagram/index.js
@@ -22,30 +22,40 @@ const KEY = 'state-diagram-input';
 const string = localStorage.getItem(KEY);
 
 if (string == null) {
+    diagram.textContent = '';
+
     const span = document.createElement('span');
     span.textContent = 'Go To ';
-    span.style.marginRight = '4px';
 
     const a = document.createElement('a');
     a.href = '../../index.html';
     a.textContent = 'APGsembly Emulator';
 
-    diagram.textContent = '';
-    diagram.append(span);
-    diagram.append(a);
+    const div = document.createElement('div');
+    div.append(span, a);
+    diagram.append(div);
 } else {
-    render(create(string));
     localStorage.removeItem(KEY);
+    render(create(string));
 }
 
-// const input = document.querySelector('#input');
+const input = document.querySelector('#input');
 
-// input.addEventListener('input', () => {
-//     try {
-//         const graphDefinition = create(input.value);
-//         render(graphDefinition);
-//         input.classList.remove('is-invalid');
-//     } catch (error) {
-//         input.classList.add('is-invalid');
-//     }
-// });
+input.addEventListener('input', () => {
+    try {
+        render(create(input.value));
+        input.classList.remove('is-invalid');
+    } catch (error) {
+        if (error instanceof Error) {
+            const lines = error.message.split("\n");
+            diagram.textContent = '';
+            diagram.append(...lines.flatMap(line => [line, document.createElement('br')]));
+        }
+        input.classList.add('is-invalid');
+    }
+});
+
+function enableInput() {
+    input.parentElement.classList.remove('d-none');
+}
+window.enableInput = enableInput;
