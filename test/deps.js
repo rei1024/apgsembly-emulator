833ed48cdb4477d2f5eb2060bc12346756585358
rei1024 <satopen1729@gmail.com>
2023-07-21 12:06:35 +0900

    chore: update deps

--- test/deps.js
+++ test/deps.js
@@ -2,7 +2,7 @@ export {
     assertEquals,
     assertIsError,
     assertThrows,
-} from "https://deno.land/std@0.192.0/testing/asserts.ts";
+} from "https://deno.land/std@0.195.0/assert/mod.ts";
 
 /**
  * @returns {never}
